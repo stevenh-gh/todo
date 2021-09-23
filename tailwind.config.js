ae6816c48018ab09eed727e16d549a4f85fa32ec
steven <steven.h@live.com>
2021-09-22 23:16:14 -0700

    Use square list instead of disc list

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,14 @@
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+    purge: [],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {},
+        listStyleType: {
+            "square": "square"
+        }
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
 }
