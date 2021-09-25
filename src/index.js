f3c56f93ae152c21b52ef1fdd0dc575635b3f805
steven <steven.h@live.com>
2021-09-24 20:10:55 -0700

    Import and call projectFormAddProjectEventListener

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import "./style.css";
 import pageLoad from "./views/pageLoad";
 import { addProjectForm } from "./views/forms";
-import { projectFormOpenEventListener, projectFormCloseEventListener } from "./eventListeners";
+import { projectFormOpenEventListener, projectFormCloseEventListener, projectFormAddProjectEventListener } from "./eventListeners";
 
 pageLoad();
 addProjectForm();
 projectFormOpenEventListener();
 projectFormCloseEventListener();
+projectFormAddProjectEventListener();
