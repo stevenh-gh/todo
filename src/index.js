c7d617b851b1bae290b54443e044971dae9a61da
steven <steven.h@live.com>
2021-09-24 19:36:28 -0700

    Call event listeners

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import "./style.css";
 import pageLoad from "./views/pageLoad";
 import { addProjectForm } from "./views/forms";
+import { projectFormOpenEventListener, projectFormCloseEventListener } from "./eventListeners";
 
 pageLoad();
 addProjectForm();
+projectFormOpenEventListener();
+projectFormCloseEventListener();
