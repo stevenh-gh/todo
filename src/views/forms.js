ed2b1527656c49371c041f6fc329b3bcdcace65e
steven <steven.h@live.com>
2021-09-24 19:35:19 -0700

    Style form

--- src/views/forms.js
+++ src/views/forms.js
@@ -4,14 +4,14 @@ export function addProjectForm() {
     const projectForm = createElement("div");
     projectForm.className = "col-start-4 col-span-6 bg-blue-100 fixed left-60 top-20 mt-2 ml-2"
     projectForm.innerHTML = `
-		<form>
-    <h1>Add project</h1>
+		<form class="p-5 space-y-3 hidden" id="project-form">
+    <h1 class="text-center text-xl">Add project</h1>
 		
-    <label for="project"><b>Project name: </b></label>
-    <input type="text" placeholder="Enter project name" name="project-name" required>
+    <label for="project-name">Project name: </label><br>
+    <input type="text" placeholder="Enter project name" name="project-name" required><br>
 		
-    <button type="submit">Add</button>
-    <button type="button" onclick="closeForm()">Close</button>
+    <button type="submit" class="bg-green-400 text-center w-full text-white font-bold">Add</button><br>
+    <button type="button" class="bg-red-500 text-center w-full text-white font-bold" id="project-form-close">Close</button>
 		</form>
 		`;
 
