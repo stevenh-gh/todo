114c63a2d7e7f0a52ac863fbc464088a9a0f801f
steven <steven.h@live.com>
2021-09-26 16:03:49 -0700

    Add serialize and a static deserialize method into class Project
    Remove creation of new project instance in Projects' add method

--- src/project.js
+++ src/project.js
@@ -1,7 +1,16 @@
 export default class Project {
+    static deserialize(serialize) {
+        const obj = JSON.parse(serialize);
+        const proj = new Project(obj.title);
+        proj.tasks = obj.tasks;
+        return proj;
+    }
     constructor(title) {
         this.title = title;
         this.tasks = [];
+    }
+    serialize() {
+        return JSON.stringify(this);
     }
 }
 
@@ -10,10 +19,9 @@ class Projects {
     constructor() {
         this.list = new Map();
     }
-    add(projectName) {
-        const p = new Project(projectName);
-        this.list.set(projectName, p);
-        localStorage.setItem(projectName, JSON.stringify(p));
+    add(project) {
+        this.list.set(project.title, project);
+        localStorage.setItem(project.title, project.serialize());
     }
     last() {
         return Array.from(this.list)[this.list.size - 1][1];
