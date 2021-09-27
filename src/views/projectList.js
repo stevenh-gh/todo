492d9585579e8cd7b36c28351bcb18aeea89eabe
steven <steven.h@live.com>
2021-09-27 14:58:33 -0700

    Rewrote projectEle's innerHTML into JS elements

--- src/views/projectList.js
+++ src/views/projectList.js
@@ -5,12 +5,16 @@ export function appendProjectToList(project) {
     const ul = document.getElementById("project-list");
     const projectEle = createElement("li");
     projectEle.classList.add("group", "relative");
-    projectEle.innerHTML = `
-    <div class="inline-flex">
-        <p class="">${project.title}</p>
-        <i class="fas fa-minus-circle text-red-500 self-center absolute right-0 text-xl hidden group-hover:block"></i>
-    </div>
-    `;
+
+    const container = createElement("div", "inline-flex");
+    const title = createElement("p");
+    title.innerText = project.title;
+    const deleteBtn = createElement("i", "fas fa-minus-circle text-red-500 self-center absolute right-0 text-xl hidden group-hover:block");
+
+    container.appendChild(title);
+    container.appendChild(deleteBtn);
+    projectEle.appendChild(container);
+
     projectEle.addEventListener("click", () => {
         toggleProjectPage(project);
     })
