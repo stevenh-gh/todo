e364fe6d0bb47ca4d6c59cc93a5866dba003b4d0
steven <steven.h@live.com>
2021-09-27 13:53:44 -0700

    Use toggleProjectPage fn in projectEle's addEventListener

--- src/views/projectList.js
+++ src/views/projectList.js
@@ -1,8 +1,12 @@
+import { toggleProjectPage } from "../eventListeners";
 import { createElement } from "./pageLoad";
 
 export function appendProjectToList(project) {
     const ul = document.getElementById("project-list");
     const projectEle = createElement("li");
     projectEle.innerText = project.title;
+    projectEle.addEventListener("click", () => {
+        toggleProjectPage(project);
+    })
     ul.appendChild(projectEle);
 }
