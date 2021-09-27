75429a9cb6360213addf8045b04f43ae8187d1de
steven <steven.h@live.com>
2021-09-27 14:48:40 -0700

    Rewrote list element's to include project title and a delete btn
    Delete button displays on a hover

--- src/views/projectList.js
+++ src/views/projectList.js
@@ -4,7 +4,13 @@ import { createElement } from "./pageLoad";
 export function appendProjectToList(project) {
     const ul = document.getElementById("project-list");
     const projectEle = createElement("li");
-    projectEle.innerText = project.title;
+    projectEle.classList.add("group", "relative");
+    projectEle.innerHTML = `
+    <div class="inline-flex">
+        <p class="">${project.title}</p>
+        <i class="fas fa-minus-circle text-red-500 self-center absolute right-0 text-xl hidden group-hover:block"></i>
+    </div>
+    `;
     projectEle.addEventListener("click", () => {
         toggleProjectPage(project);
     })
