d8c85dd02f35f1b815d3f575e47c4e4e1ae013da
steven <steven.h@live.com>
2021-09-28 15:08:50 -0700

    Call addTaskForm from index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import "./style.css";
 import pageLoad from "./views/pageLoad";
-import { addProjectForm } from "./views/forms";
+import { addProjectForm, addTaskForm } from "./views/forms";
 import { projectFormOpenEventListener, projectFormCloseEventListener, projectFormAddProjectEventListener } from "./eventListeners";
 
 pageLoad();
 addProjectForm();
+addTaskForm();
 projectFormOpenEventListener();
 projectFormCloseEventListener();
 projectFormAddProjectEventListener();
