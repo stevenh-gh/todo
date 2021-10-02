70ff18f4e5e9dfd09adc5e1878349aabb0d8887e
steven <steven.h@live.com>
2021-10-02 14:32:56 -0700

    Add and export fn appendTask that append task to the task space when adding a new task

--- src/views/loadTasks.js
+++ src/views/loadTasks.js
@@ -14,3 +14,16 @@ export default function loadTasks(project) {
         taskSpace.appendChild(taskDiv);
     });
 }
+
+export function appendTask(task) {
+    const taskSpace = document.getElementById("task-space");
+    const taskDiv = createElement("div", "col-start-4 col-span-6 rounded-md p-4 border border-gray-300 shadow-lg", task.title);
+    taskDiv.innerHTML = `
+        			<label for="" class="flex justify-between">
+                        <input type="checkbox" class="transform scale-150 self-center">
+                        <span class="text-3xl">${task.title}</span>
+                        <span class="text-3xl">${task.dueDate}</span>
+                    </label>
+                    `;
+    taskSpace.appendChild(taskDiv);
+}
