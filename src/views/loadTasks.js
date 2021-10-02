3e330d8e42b01125dd8f8d01b85c900687e5f62b
steven <steven.h@live.com>
2021-10-02 14:10:47 -0700

    Add fn loadTasks to load all tasks when project is clicked on

---
+++ src/views/loadTasks.js
@@ -0,0 +1,16 @@
+import { createElement } from "./pageLoad";
+
+export default function loadTasks(project) {
+    const taskSpace = document.getElementById("task-space");
+    project.tasks.forEach(task => {
+        const taskDiv = createElement("div", "col-start-4 col-span-6 rounded-md p-4 border border-gray-300 shadow-lg", task.title);
+        taskDiv.innerHTML = `
+        			<label for="" class="flex justify-between">
+                        <input type="checkbox" class="transform scale-150 self-center">
+                        <span class="text-3xl">${task.title}</span>
+                        <span class="text-3xl">${task.dueDate}</span>
+                    </label>
+                    `;
+        taskSpace.appendChild(taskDiv);
+    });
+}
