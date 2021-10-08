13fb92c3ac89f720914f1933b93120f7dc4b0f58
steven <steven.h@live.com>
2021-10-07 17:22:33 -0700

    Change taskDiv from HTML to JS

--- src/views/loadTasks.js
+++ src/views/loadTasks.js
@@ -7,13 +7,26 @@ export default function loadTasks(project) {
     const taskSpace = document.getElementById("task-space");
     project.tasks.forEach(task => {
         const taskDiv = createElement("div", "col-start-4 col-span-6 rounded-md p-4 border border-gray-300 shadow-lg transform hover:scale-105 group", task.title);
-        taskDiv.innerHTML = `
-        			<label for="" class="flex justify-between group-hover:cursor-pointer">
-                        <input type="checkbox" class="transform scale-150 self-center">
-                        <span class="text-3xl">${task.title}</span>
-                        <span class="text-3xl">${task.dueDate}</span>
-                    </label>
-                    `;
+        // taskDiv.innerHTML = `
+        // 			<label for="" class="flex justify-between group-hover:cursor-pointer">
+        //                 <input type="checkbox" class="transform scale-150 self-center">
+        //                 <span class="text-3xl">${task.title}</span>
+        //                 <span class="text-3xl">${task.dueDate}</span>
+        //             </label>
+        //             `;
+
+        const label = createElement("div", "flex justify-between group-hover:cursor-pointer");
+        const input = createElement("input", "transform scale-150 self-center");
+        input.setAttribute("type", "checkbox");
+        const span1 = createElement("span", "text-3xl");
+        span1.innerText = task.title;
+        const span2 = createElement("span", "text-3xl");
+        span2.innerText = task.dueDate;
+        label.appendChild(input);
+        label.appendChild(span1);
+        label.appendChild(span2);
+        taskDiv.appendChild(label);
+
         const deleteBtn = createElement("i", "fa fa-minus-circle text-red-500 hidden group-hover:block group-hover:cursor-pointer");
         deleteBtn.addEventListener("click", () => {
             project.deleteTask(task);
@@ -33,13 +46,26 @@ export default function loadTasks(project) {
 export function appendTask(task) {
     const taskSpace = document.getElementById("task-space");
     const taskDiv = createElement("div", "col-start-4 col-span-6 rounded-md p-4 border border-gray-300 shadow-lg transform hover:scale-105 group", task.title);
-    taskDiv.innerHTML = `
-        			<label for="" class="flex justify-between group-hover:cursor-pointer">
-                        <input type="checkbox" class="transform scale-150 self-center">
-                        <span class="text-3xl">${task.title}</span>
-                        <span class="text-3xl">${task.dueDate}</span>
-                    </label>
-                    `;
+    // taskDiv.innerHTML = `
+    //     			<label for="" class="flex justify-between group-hover:cursor-pointer">
+    //                     <input type="checkbox" class="transform scale-150 self-center">
+    //                     <span class="text-3xl">${task.title}</span>
+    //                     <span class="text-3xl">${task.dueDate}</span>
+    //                 </label>
+    //                 `;
+
+    const label = createElement("div", "flex justify-between group-hover:cursor-pointer");
+    const input = createElement("input", "transform scale-150 self-center");
+    input.setAttribute("type", "checkbox");
+    const span1 = createElement("span", "text-3xl");
+    span1.innerText = task.title;
+    const span2 = createElement("span", "text-3xl");
+    span2.innerText = task.dueDate;
+    label.appendChild(input);
+    label.appendChild(span1);
+    label.appendChild(span2);
+    taskDiv.appendChild(label);
+
     const deleteBtn = createElement("i", "fa fa-minus-circle text-red-500 hidden group-hover:block group-hover:cursor-pointer");
     const project = projects.list.get(taskSpace.parentElement.children[0].childNodes[1].textContent);
     deleteBtn.addEventListener("click", () => {
